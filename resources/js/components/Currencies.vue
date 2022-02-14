1e12470c7247350ec7dc2a5a81ffe129984f56a7
Salim Djerbouh <caddydz4@gmail.com>
2022-02-14 20:39:03 +0100

    Frontend (select base currency)
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- resources/js/components/Currencies.vue
+++ resources/js/components/Currencies.vue
@@ -5,6 +5,33 @@
         <div class="card">
           <div class="card-header">Currencies</div>
           <div class="card-body">
+            <div class="dropdown" v-if="currencies">
+                <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Base Currency
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <button class="dropdown-item" v-for="currency in currencies" :key="currency.id"
+                        @click="changeBaseCurrency(currency.id)">
+                        {{ currency.name }}
+                    </button>
+                </div>
+            </div>
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Average rate</th>
+                  <th>Maximum rate</th>
+                  <th>Minimum rate</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{{ average_rate }}</td>
+                  <td> {{ maximum_rate }}</td>
+                  <td>{{ minimum_rate }}</td>
+                </tr>
+              </tbody>
+            </table>
             <table v-if="currencies" class="table table-bordered">
               <thead>
                 <tr>
@@ -14,8 +41,11 @@
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="currency in currencies">
-                  <td>{{ currency.name }}</td>
+                <tr v-for="currency in currencies" :key="currency.id">
+                  <td>
+                    {{ currency.name }}
+                    <a :href="`/history/${currency.id}`">View history</a>
+                  </td>
                   <td>{{ currency.nominal }}</td>
                   <td>{{ currency.rate }}</td>
                 </tr>
@@ -33,14 +63,34 @@ export default {
 
   data: () => ({
     currencies: null,
+    average_rate: null,
+    maximum_rate: null,
+    minimum_rate: null
   }),
+
+  methods: {
+      changeBaseCurrency(currency) {
+          axios.get(`/api/currency?base_currency_id=${currency}`)
+            .then((response) => {
+                this.currencies = response.data.currencies;
+            });
+      }
+  },
 
   async mounted() {
     console.log('Component mounted.')
 
-    const { data: { data } } = await axios.get('/api/currency')
+    const { data: {
+        currencies,
+        average_rate,
+        maximum_rate,
+        minimum_rate
+    } } = await axios.get('/api/currency')
 
-    this.currencies = data
+    this.currencies = currencies
+    this.average_rate = average_rate
+    this.minimum_rate = minimum_rate
+    this.maximum_rate = maximum_rate
   },
 }
 </script>
